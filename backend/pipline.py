629326b8f2f6411e0f49ac7e1211ee190ebf1863
r11725042 <r11725042@ntu.edu.tw>
2023-05-15 02:26:32 +0800

    add pipline

--- backend/pipline.py
+++ backend/pipline.py
@@ -5,4 +5,34 @@ from pedalboard.io import AudioFile
 # Make a Pedalboard object, containing multiple audio plugins:
 
 
-board = Pedalboard([Chorus(), Reverb(room_size=0.25)])
+def choose_function(func_dict):
+    def decorator_func(func):
+        def wrapper(*args, **kwargs):
+            key = args[0]  # 假設第一個參數為選擇鍵
+            if key in func_dict:
+                chosen_func = func_dict[key]
+                return chosen_func(*args[1:], **kwargs)
+            else:
+                raise ValueError("Invalid input.")
+        return wrapper
+    return decorator_func
+
+
+@choose_function({
+    "Chorus": Chorus,
+    "Reverb": Reverb
+    # 之後有新功能可以再加
+})
+def effect_handler(effect, input):
+    return True
+
+
+def effect_List_handler(request_json):
+    effect_params = []
+    for effect_setting in request_json:
+        effect_params.append(effect_handler(effect_setting))
+    return effect_params
+
+
+request_json = {}
+board = Pedalboard(effect_List_handler(request_json))
