197477e36b156e56c5e1e978fc7bf8f0828e1694
yoyo <yoyo0918887333@gmail.com>
2023-06-01 20:36:59 +0800

    frontend update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,104 +3,176 @@ import { DropzoneArea } from "material-ui-dropzone";
 import { useState, React } from "react";
 
 function App() {
-  const [fileList, setFileList] = useState([]);
-  const [echo, setecho] = useState(false);
-  const [volume, setvolume] = useState(100);
+	const [fileList, setFileList] = useState([]);
+	const [volume, setvolume] = useState(100);
+	const [Phaser, setPhaser] = useState(false);
+	const [Chorus, setChorus] = useState(false);
+	const [Reverb, setReverb] = useState(0);
+	const [PitchShift, setPitchShift] = useState(0);
+	const [LadderFilter, setLadderFilter] = useState(0);
+	const [Distortion, setDistortion] = useState(false);
 
-  const handleFileChange = (files) => {
-    setFileList(files);
-  };
+	const handleFileChange = (files) => {
+		setFileList(files);
+	};
 
-  const handleEchoChange = () => {
-    setecho(!echo);
-  };
+	const handleUploadClick = () => {
+		if (!fileList) {
+			return;
+		}
 
-  const handleValueChange = (e) => {
-    setvolume(e.target.value);
-  };
+		const data = new FormData();
+		files.forEach((file) => {
+			data.append(`file`, file, file.name);
+			data.append(`controls`, {
+				Volume: volume,
+				Reverb: Reverb,
+				PitchShift: PitchShift,
+				LadderFilter: LadderFilter,
+				Distortion: Distortion,
+				Phaser: Phaser,
+				Chorus: Chorus,
+			});
 
-  const handleUploadClick = () => {
-    if (!fileList) {
-      return;
-    }
+			fetch("/api/", {
+				method: "POST",
+				body: data,
+			})
+				.then((res) => {
+					if (res.status === 404) return;
+					return res.blob();
+				})
+				.then((blob) => {
+					console.log(blob);
+					const url = window.URL.createObjectURL(blob);
+					const a = document.createElement("a");
+					a.style.display = "none";
+					a.href = url;
+					a.download = file.name;
+					document.body.appendChild(a);
+					a.click();
+					window.URL.revokeObjectURL(url);
+					alert("your file has downloaded!"); // or you know, something with better UX...
+				})
+				.catch(() => alert("oh no!"));
+		});
+	};
 
-    const data = new FormData();
-    files.forEach((file) => {
-      data.append(`file`, file, file.name);
-      data.append(`echo`, echo);
-      data.append(`volume`, volume);
-      fetch("/api/", {
-        method: "POST",
-        body: data,
-      })
-        .then((res) => res.blob())
-        .then((blob) => {
-          const url = window.URL.createObjectURL(blob);
-          const a = document.createElement("a");
-          a.style.display = "none";
-          a.href = url;
-          a.download = file.name;
-          document.body.appendChild(a);
-          a.click();
-          window.URL.revokeObjectURL(url);
-          alert("your file has downloaded!"); // or you know, something with better UX...
-        })
-        .catch(() => alert("oh no!"));
-      console.log(data);
-    });
-  };
+	function Editor() {
+		return (
+			<div className="Edit">
+				<div className="row">
+					<input
+						type="range"
+						id="volume"
+						name="volume"
+						onChange={(e) => setvolume(e.target.value)}
+						value={volume}
+						step={1}
+						min={0}
+						max={200}
+					/>
+					<label htmlFor="volume">Volume {volume}%</label>
+				</div>
+				<div className="row">
+					<input
+						type="range"
+						id="Reverb"
+						name="Reverb"
+						step={1}
+						value={Reverb}
+						onChange={(e) => setReverb(e.target.value)}
+						min={0}
+						max={100}
+					/>
+					<label htmlFor="volume">Reverb {Reverb}%</label>
+				</div>
+				<div className="row">
+					<input
+						type="range"
+						id="PitchShift"
+						name="PitchShift"
+						step={1}
+						value={PitchShift}
+						onChange={(e) => setPitchShift(e.target.value)}
+						min={-15}
+						max={15}
+					/>
+					<label htmlFor="PitchShift">
+						PitchShift {PitchShift} semitones
+					</label>
+				</div>
+				<div className="row">
+					<input
+						type="range"
+						id="LadderFilter"
+						name="LadderFilter"
+						step={1}
+						value={LadderFilter}
+						onChange={(e) => setLadderFilter(e.target.value)}
+						min={0}
+						max={1600}
+					/>
+					<label htmlFor="LadderFilter">
+						LadderFilter cutoff {LadderFilter}hz
+					</label>
+				</div>
+				<div className="row">
+					<input
+						type="checkbox"
+						id="Chorus"
+						name="Chorus"
+						onChange={(e) => setChorus(e.target.checked)}
+						checked={Chorus}
+					/>
+					<label htmlFor="Chorus">Chorus</label>
+				</div>
+				<div className="row">
+					<input
+						type="checkbox"
+						id="Phaser"
+						name="Phaser"
+						onChange={(e) => setPhaser(e.target.checked)}
+						checked={Phaser}
+					/>
+					<label htmlFor="Phaser">Phaser</label>
+				</div>
+				<div className="row">
+					<input
+						type="checkbox"
+						id="Distortion"
+						name="Distortion"
+						onChange={(e) => setDistortion(e.target.checked)}
+						checked={Distortion}
+					/>
+					<label htmlFor="Distortion">Distortion</label>
+				</div>
+				<input
+					className="submit"
+					type="submit"
+					value={"上傳"}
+					onClick={handleUploadClick}
+				/>
+			</div>
+		);
+	}
 
-  function Editor() {
-    return (
-      <div className="Edit">
-        <div className="row">
-          <input
-            type="range"
-            id="volume"
-            name="volume"
-            onChange={handleValueChange}
-            value={volume}
-            min={0}
-            max={200}
-          />
-          <label htmlFor="volume">音量 {volume}%</label>
-        </div>
-        <div className="row">
-          <input
-            type="checkbox"
-            id="echo"
-            name="echo"
-            onChange={handleEchoChange}
-            checked={echo}
-          />
-          <label htmlFor="echo">回響</label>
-        </div>
-        <input
-          className="submit"
-          type="submit"
-          value={"上傳"}
-          onClick={handleUploadClick}
-        />
-      </div>
-    );
-  }
+	// 👇 files is not an array, but it's iterable, spread to get an array of files
+	const files = fileList ? [...fileList] : [];
 
-  // 👇 files is not an array, but it's iterable, spread to get an array of files
-  const files = fileList ? [...fileList] : [];
-
-  return (
-    <div className="App">
-      <h1 className="webname">Online Pedalboard</h1>
-      <DropzoneArea
-        acceptedFiles={["audio/*"]}
-        dropzoneText={"Drag and drop audios here or click"}
-        onChange={(files) => handleFileChange(files)}
-        maxFileSize={9000000}
-        className="Upload"
-      />
-      <Editor />
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1 className="webname">Online Pedalboard</h1>
+			<DropzoneArea
+				acceptedFiles={["audio/*"]}
+				dropzoneText={"Drag and drop audios here or click"}
+				onChange={(files) => handleFileChange(files)}
+				maxFileSize={9000000}
+				className="Upload"
+			/>
+			<Editor />
+		</div>
+	);
 }
 
 export default App;
